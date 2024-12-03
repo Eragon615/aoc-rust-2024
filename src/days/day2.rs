c2e602a2b97678dc8e22c2faff9291cd9ea64385
Eragon615 <dragonmasterclay@gmail.com>
2024-12-02 18:07:12 -0600

    refactors and completes d2p2

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -3,84 +3,66 @@ use crate::Application;
 
 impl Application {
     pub fn day2(self) {
+        let mut reports: Vec<Vec<i32>> = Vec::new();
+        for i in 0..self.input.len() {
+            reports.push(Vec::new());
+            let mut line = self.input[i].split_whitespace();
+            while let Some(value) = line.next() {
+                reports[i].push(value.parse().unwrap());
+            }
+        }
         if self.args.part == 1 {
-            self.d2p1();
+            self.d2p1(reports);
         } else {
-            self.d2p2();
+            self.d2p2(reports);
         }
     }
 
-    fn d2p1(self) {
-        let mut levels: Vec<Vec<i32>> = Vec::new();
-        for i in 0..self.input.len() {
-            levels.push(Vec::new());
-            let mut line = self.input[i].split_whitespace();
-            while let Some(value) = line.next() {
-                levels[i].push(value.parse().unwrap());
-            }
-        }
+    fn d2p1(self, reports: Vec<Vec<i32>>) {
         let mut answer = 0; 
-        for reading in levels {
-            let mut valid = true;
-            let mut sign = None;
-            for i in 1..reading.len() {
-                let variance = reading[i] - reading[i-1];
-                match sign {
-                    None => {sign = Some(variance.signum())},
-                    Some(pos_neg) => {
-                        if pos_neg != variance.signum() {
-                            valid = false;
-                        };
-                    }
-                };
-                if variance.abs() != 1 && variance.abs() != 2 && variance.abs() != 3 {
-                    valid = false;
-                }
-            }
-            if valid {
+        for levels in reports {
+            if is_valid(&levels) {
                 answer += 1;
             }
         }
         println!("{answer}");
     }
 
-    fn d2p2(self) {
-        let mut levels: Vec<Vec<i32>> = Vec::new();
-        for i in 0..self.input.len() {
-            levels.push(Vec::new());
-            let mut line = self.input[i].split_whitespace();
-            while let Some(value) = line.next() {
-                levels[i].push(value.parse().unwrap());
-            }
-        }
+    fn d2p2(self, reports: Vec<Vec<i32>>) {
         let mut answer = 0; 
-        for reading in levels {
-            let mut bad_level = 0;
-            let mut positives = 0;
-            let mut negatives = 0;
-            let mut valid = true;
-            for i in 1..reading.len() {
-                let variance = reading[i] - reading[i-1];
-                if variance.signum() == 1 {
-                    positives += 1;
-                } else {
-                    negatives += 1;
-                }
-                if variance.abs() != 1 && variance.abs() != 2 && variance.abs() != 3 {
-                    bad_level += 1;
-                }
-            }
-            let sign_change:i32 = positives - negatives;
-            if sign_change.abs() == reading.len() as i32 && bad_level <= 1 {
-            } else if reading.len() as i32 - sign_change.abs() == 1  && bad_level == 0 {
+        for levels in reports {
+            if is_valid(&levels) {
+                answer += 1;
             } else {
-                valid = false;
-            }
-            println!("{reading:?} {sign_change} {bad_level}");
-            if valid {
-                answer += 1;
+                for i in 0..levels.len() {
+                    let mut new_levels = levels.clone();
+                    new_levels.remove(i);
+                    if is_valid(&new_levels) {
+                        answer += 1;
+                        break;
+                    } 
+                }
             }
         }
         println!("{answer}");
     }
+}
+
+fn is_valid(levels: &Vec<i32>) -> bool {
+    let mut sign = None;
+    for i in 1..levels.len() {
+        let variance = levels[i] - levels[i-1];
+        match sign {
+            None => {sign = Some(variance.signum())},
+            Some(pos_neg) => {
+                if pos_neg != variance.signum() {
+                    return false;
+                };
+            }
+        };
+        if variance.abs() != 1 && variance.abs() != 2 && variance.abs() != 3 {
+            return false;
+        }
+    }
+    return true;
 }
