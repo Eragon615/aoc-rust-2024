d1b97ef09d2b3e3e28120a86d0083b975677d9e2
Eragon615 <dragonmasterclay@gmail.com>
2024-12-09 17:47:07 -0600

    day8 complete!

--- src/days/day8.rs
+++ src/days/day8.rs
@@ -38,8 +38,34 @@ impl Application {
         println!("{}", hash.len());
     }
 
-    fn d8p2(self) {}
+    fn d8p2(self) {
+        let mut btree = BTreeMap::new();
+        let mut hash = HashSet::new();
+        let roof = CharMap::new_from_input(&self.input);
+        roof.gather_points(&mut btree, &vec!['.']);
+        for (_, points) in btree {
+            let mut pairs = Vec::new();
+            for i in 0..points.len() - 1 {
+                for j in i+1..points.len() {
+                    pairs.push((points[i], points[j]));
+                }
+            }
+            for pair in pairs {
+                let distance = pair.0 - pair.1;
+                let mut new_point = pair.1 - distance;
+                while roof.is_in_bounds(new_point) {
+                    _ = hash.insert(new_point);
+                    new_point = new_point - distance;
+                }
+                new_point = pair.0 - (distance * Point::new(-1, -1));
+                while roof.is_in_bounds(new_point) {
+                    _ = hash.insert(new_point);
+                    new_point = new_point - (distance * Point::new(-1, -1));
+                }
+                _ = hash.insert(pair.0);
+                _ = hash.insert(pair.1);
+            }
+        }
+        println!("{}", hash.len());
+    }
 }
-
-// use point math to generate new points 
-// add new points to hashset to eliminate duplicates
