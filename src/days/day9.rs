f3116702d926f9f475ce3b387c58c98d5c06c48a
Eragon615 <dragonmasterclay@gmail.com>
2024-12-10 00:00:03 -0600

    project sync

--- src/days/day9.rs
+++ src/days/day9.rs
@@ -2,115 +2,84 @@ use crate::Application;
 
 impl Application {
     pub fn day9(self) {
-        let oasis = parse_input(&self.input);
         if self.args.part == 1 {
-            self.d9p1(oasis);
+            self.d9p1();
         } else {
-            self.d9p2(oasis);
+            self.d9p2();
         }
     }
 
-    fn d9p1(self, oasis: Vec<Vec<i64>>) {
+    fn d9p1(self) {
         let mut answer = 0;
-        for num_vec in oasis {
-            answer += find_next(&num_vec);
-        }
+        let mut diskmap = Diskmap::new_from_input(self.input);
+        diskmap.defrag();
         println!("{answer}");
     }
 
-    fn d9p2(self, oasis: Vec<Vec<i64>>) {
+    fn d9p2(self) {
         let mut answer = 0;
-        for num_vec in oasis {
-            answer += find_prev(&num_vec);
-        }
+
         println!("{answer}");
     }
 }
 
-fn parse_input(input: &Vec<String>) -> Vec<Vec<i64>> {
-    let mut output = Vec::new();
-    for line in input {
-        output.push(
-            line.clone()
-                .split_whitespace()
-                .map(|n| n.parse().expect("Number parsing failed"))
-                .collect::<Vec<i64>>(),
-        );
-    }
-    return output;
+struct Diskmap {
+    pos: usize,
+    disk: Vec<Block>
 }
 
-fn find_next(input: &Vec<i64>) -> i64 {
-    let output;
-    let mut deepness = 0;
-    let mut working_vec = Vec::new();
-    working_vec.push(input.clone());
-    loop {
-        let new_line = next_row(&working_vec[deepness]);
-        deepness += 1;
-        working_vec.push(new_line);
-        if all_zero(&working_vec[deepness]) {
-            break;
-        }
-    }
-    loop {
-        let curlast = working_vec[deepness].last().unwrap_or(&0);
-        let prevlast = working_vec[deepness - 1]
-            .last()
-            .expect("We didn't go deep enough");
-        let nextnum = curlast + prevlast;
-        deepness -= 1;
-        if deepness == 0 {
-            output = nextnum;
-            break;
-        }
-        working_vec[deepness].push(nextnum);
-    }
-    return output;
+#[derive(Debug,Clone, Copy)]
+enum Block {
+    Filespace(Filespace),
+    Freespace(Freespace)
 }
 
-fn find_prev(input: &Vec<i64>) -> i64 {
-    let output;
-    let mut deepness = 0;
-    let mut working_vec = Vec::new();
-    working_vec.push(input.clone());
-    loop {
-        let new_line = next_row(&working_vec[deepness]);
-        deepness += 1;
-        working_vec.push(new_line);
-        if all_zero(&working_vec[deepness]) {
-            break;
-        }
-    }
-    loop {
-        let curfirst = working_vec[deepness].first().unwrap_or(&0);
-        let prevfirst = working_vec[deepness - 1]
-            .first()
-            .expect("Backed into a cop");
-        let nextnum = prevfirst - curfirst;
-        deepness -= 1;
-        if deepness == 0 {
-            output = nextnum;
-            break;
-        }
-        working_vec[deepness].insert(0, nextnum);
-    }
-    return output;
+
+#[derive(Debug,Clone, Copy)]
+struct Filespace {
+    id_num: usize,
+    length: usize
 }
 
-fn next_row(input: &Vec<i64>) -> Vec<i64> {
-    let mut output = Vec::new();
-    for i in 0..(input.len() - 1) {
-        output.push(input[i + 1] - input[i]);
-    }
-    return output;
+
+#[derive(Debug,Clone, Copy)]
+struct Freespace {
+    length: usize
 }
 
-fn all_zero(input: &Vec<i64>) -> bool {
-    for i in input {
-        if *i != 0 {
-            return false;
+impl Diskmap {
+    fn new_from_input(input: Vec<String>) -> Self {
+        let mut disk = Vec::new();
+        let mut id_num = 0;
+        for line in input {
+            for (idx, num) in line.chars().enumerate() {
+                if idx % 2 == 0 {
+                    disk.push(Block::Filespace(Filespace{id_num, length: num.to_digit(10).expect("A letter snuck into the puzzle input.") as usize }));
+                    id_num += 1;
+                } else {
+                    disk.push(Block::Freespace(Freespace { length: num.to_digit(10).expect("A letter snuck into the puzzle input.") as usize }));
+                }
+            }
+        }
+        return Self{pos: 0, disk};
+    }
+
+    fn defrag(&mut self) {
+        let mut remainder: Option<Filespace> = None;
+        let mut current_file: Filespace;
+        for block in self.disk.iter() {
+            if let Block::Freespace(free) = block {
+                if let Some(leftover) = &remainder {
+                    current_file = *leftover;
+                } else {
+                    while let Some(last_block) = self.disk.last_mut() {
+                        if let Block::Freespace(_) = last_block {
+
+                        }
+                    }
+                }
+            }
         }
     }
-    return true;
+
 }
